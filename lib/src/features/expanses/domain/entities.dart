fbb2171628e6084af8be669db24fa4dea2898a15
pablo <pablo.silva@warren.com.br>
2021-11-01 18:16:58 -0300

    test: covering expense use case test

--- lib/src/features/expanses/domain/entities.dart
+++ lib/src/features/expanses/domain/entities.dart
@@ -1,24 +1,39 @@
+import 'package:equatable/equatable.dart';
 import 'package:tip/src/features/budgets/domain/budget_entitites.dart';
 
 /// a list of [Transaction] related to a [Budget] is the definition of
-class BudgetTransactions {
+class BudgetTransactions extends Equatable {
   final List<Transaction> transactions;
   final Budget budget;
 
-  BudgetTransactions({required this.transactions, required this.budget});
+  @override
+  List<Object?> get props => [
+        transactions,
+        budget,
+      ];
+
+  const BudgetTransactions({required this.transactions, required this.budget});
 }
 
 /// A financial transaction on user account
-class Transaction {
-  Transaction({
+class Transaction extends Equatable {
+  final double value;
+  final String name;
+  final String id;
+  final DateTime date;
+
+  @override
+  List<Object?> get props => [
+        value,
+        name,
+        id,
+        date,
+      ];
+
+  const Transaction({
     required this.value,
     required this.name,
     required this.id,
     required this.date,
   });
-
-  final double value;
-  final String name;
-  final String id;
-  final DateTime date;
 }
